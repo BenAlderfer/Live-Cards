390159a27db49f5175ba675e34b8bbaa28a9d1db
Ben Alderfer <benalderfer974@gmail.com>
2016-10-11 15:21:35 -0400

    add rest of permission stuff (not working yet)

--- app/src/main/java/edu/rit/sse/livecards/MainActivity.java
+++ app/src/main/java/edu/rit/sse/livecards/MainActivity.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
@@ -19,6 +21,8 @@ import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private final int PERMISSION_REQUEST_CAMERA_CODE = 1;
+
     private static final String TAG = MainActivity.class.getSimpleName();
     private DecoratedBarcodeView barcodeView;
 
@@ -50,12 +54,32 @@ public class MainActivity extends AppCompatActivity {
             //they have not previously denied access
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
                     shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        PERMISSION_REQUEST_CAMERA_CODE);
             }
         }
 
         barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
         barcodeView.decodeContinuous(callback);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CAMERA_CODE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, move on
+                } else {
+                    // permission denied, yell at them
+                    Toast.makeText(this, "You must grant the camera permission " +
+                            "for this app to function.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
     }
 
     @Override
